d00ab5e4aa24c83d3db208da42c499adeb8077eb
Peng Mei Yu <i@pengmeiyu.com>
2020-11-13 10:18:43 +0800

    shanghai: New server.

---
+++ shanghai/os.scm
@@ -0,0 +1,109 @@
+;;; Copyright © 2020 Peng Mei Yu <pengmyu@gmail.com>
+
+(use-modules (gnu)
+             (gnu packages)
+             (gnu system)
+             (guix store))
+
+(use-package-modules shells)
+(use-service-modules networking nix ssh sysctl web)
+
+(include "web.scm")
+
+
+(define %motd
+  (plain-file "motd"
+              "\x1b[1;37mWelcome to the Guix China server!\x1b[0m\n\n"))
+
+(define %ssh-public-key
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGixg7L7vRFgmxBS2GmI4/UqPw7pERi3qbKFUPaEZIF meiyu")
+
+(define %substitute-urls
+  '("https://mirror.guix.org.cn"
+    "https://ci.guix.gnu.org"))
+
+(define-public %packages
+  (append
+   %base-packages
+   (map (compose list specification->package+output)
+        '("bind:utils" "btrfs-progs" "certbot" "curl" "dosfstools" "emacs"
+          "fish" "git" "gnupg" "guile-readline" "htop" "iftop" "ncurses"
+          "nftables" "nss-certs" "openssh" "pinentry" "python" "rsync" "stow"
+          "tmux" "tree" "wget" "zsh"))))
+
+(define %services
+  (append
+   (list
+    (service dhcp-client-service-type)
+    (service nftables-service-type
+             (nftables-configuration (ruleset (local-file "nftables.conf"))))
+    (service nix-service-type)
+    (service openssh-service-type
+             (openssh-configuration
+              (permit-root-login 'without-password)
+              (password-authentication? #f)
+              (authorized-keys
+               `(("root" ,(plain-file "authorized_keys"
+                                      %ssh-public-key))
+                 ("meiyu" ,(plain-file "authorized_keys"
+                                       %ssh-public-key))))))
+    (service sysctl-service-type
+             (sysctl-configuration
+              (settings '(("net.core.default_qdisc" . "fq")
+                          ("net.ipv4.tcp_congestion_control" . "bbr")))))
+    (service guix-publish-service-type
+             (guix-publish-configuration
+              (port 8181)
+              (cache "/var/cache/guix/publish")
+              (compression '(("lzip" 9)))
+              (ttl (* 30 24 60 60)))))
+   %web-services
+   (modify-services %base-services
+     (guix-service-type
+      config => (guix-configuration
+                 (inherit config)
+                 (substitute-urls %substitute-urls)
+                 (extra-options '("--max-jobs" "4"))))
+     (login-service-type
+      config => (login-configuration
+                 (inherit config)
+                 (motd %motd))))))
+
+(operating-system
+  (host-name "shanghai.guix.org.cn")
+  (timezone "Asia/Shanghai")
+  (locale "en_US.UTF-8")
+
+  (firmware '())
+
+  (bootloader (bootloader-configuration
+               (bootloader grub-bootloader)
+               (target "/dev/vda")))
+
+  (file-systems (cons* (file-system
+                         (device "/dev/vda1")
+                         (mount-point "/")
+                         (type "ext4"))
+                       (file-system
+                         (device "/dev/vdb1")
+                         (mount-point "/srv")
+                         (type "btrfs")
+                         (options "compress=zstd"))
+                       %base-file-systems))
+
+  (users (cons (user-account
+                (name "meiyu")
+                (comment "Peng Mei Yu")
+                (group "users")
+                (supplementary-groups '("wheel"))
+                (shell (file-append zsh "/bin/zsh")))
+               %base-user-accounts))
+
+  (hosts-file
+   (plain-file "hosts"
+               (string-append (local-host-aliases host-name)
+                              "127.0.0.1 mirror.guix.org.cn")))
+
+  (packages %packages)
+
+  (services %services))
---
+++ shanghai/web.scm
@@ -0,0 +1,197 @@
+;;; Copyright © 2020 Peng Mei Yu <i@pengmeiyu.com>
+
+(use-modules (gnu packages version-control)
+             (gnu packages web)
+             (gnu services admin)
+             (gnu services certbot)
+             (gnu services mcron)
+             (gnu services version-control)
+             (gnu services web)
+             (guix packages)
+             (guix utils))
+
+(define %nginx-package
+  (package
+    (inherit nginx)
+    (arguments
+     (substitute-keyword-arguments (package-arguments nginx)
+       ((#:configure-flags flags ''())
+        `(cons "--with-http_stub_status_module" ,flags))))))
+
+(define %nginx-reload
+  (program-file "nginx-reload"
+                #~(let ((pid (call-with-input-file "/var/run/nginx/pid"
+                               read)))
+                    (kill pid SIGHUP))))
+
+(define %certbot-configuration
+  (certbot-configuration
+   (email "admin@guix.org.cn")
+   (certificates (list (certificate-configuration
+                        (domains '("guix.org.cn"
+                                   "www.guix.org.cn"))
+                        (deploy-hook %nginx-reload))
+                       (certificate-configuration
+                        (domains '("ci.guix.org.cn"))
+                        (deploy-hook %nginx-reload))
+                       (certificate-configuration
+                        (domains '("mirror.guix.org.cn"))
+                        (deploy-hook %nginx-reload))))))
+
+(define %git-http-nginx-location-configuration
+  (git-http-nginx-location-configuration
+   (git-http-configuration
+    (export-all? #t)
+    (git-root "/srv/git")
+    (uri-path "/git/"))))
+
+(define %git-repository-mirror-jobs
+  (list #~(job '(next-minute (range 0 60 10))
+               #$(program-file
+                  "git-mirror-job"
+                  #~(begin
+                      (setenv "GIT_SSL_CAINFO" "/etc/ssl/certs/ca-certificates.crt")
+                      (system* #$(file-append git "/bin/git")
+                               "--git-dir=/srv/git/guix.git"
+                               "fetch"))))))
+
+(define %guix-mirror-nginx-location-configurations
+  (let ((upstream "guix-mirror.pengmeiyu.com"))
+    (list (nginx-location-configuration
+           (uri "~ \\.narinfo$")
+           (body (list
+                  (string-append "set $upstream \"https://" upstream "\";")
+                  "proxy_pass $upstream;"
+                  "proxy_ssl_server_name on;"
+                  (string-append "proxy_ssl_name " upstream ";")
+                  (string-append "proxy_set_header Host " upstream ";")
+
+                  "proxy_cache narinfo;"
+                  "proxy_cache_valid 200 206 60d;"
+                  "proxy_cache_valid any 5m;"
+                  "proxy_connect_timeout 10s;"
+                  "proxy_read_timeout 10s;"
+                  "proxy_send_timeout 10s;"
+                  "client_body_buffer_size 128k;"
+
+                  "proxy_ignore_client_abort on;"
+                  "proxy_hide_header Set-Cookie;"
+                  "proxy_ignore_headers Set-Cookie;")))
+          (nginx-location-configuration
+           (uri "~ ^/nix-cache-info|^/nar/")
+           (body (list
+                  (string-append "set $upstream \"https://" upstream "\";")
+                  "proxy_pass $upstream;"
+                  "proxy_ssl_server_name on;"
+                  (string-append "proxy_ssl_name " upstream ";")
+                  (string-append "proxy_set_header Host " upstream ";")
+
+                  "proxy_cache nar;"
+                  "proxy_cache_valid 200 206 60d;"
+                  "proxy_cache_valid any 5m;"
+                  "proxy_connect_timeout 60s;"
+                  "proxy_read_timeout 60s;"
+                  "proxy_send_timeout 60s;"
+                  "client_body_buffer_size 256k;"
+                  "gzip off;"
+
+                  "proxy_ignore_client_abort on;"
+                  "proxy_hide_header Set-Cookie;"
+                  "proxy_ignore_headers Set-Cookie;"))))))
+
+(define %nginx-status-stub-configuration
+  (nginx-location-configuration
+   (uri "/nginx_status")
+   (body '("stub_status on;"
+           "access_log off;"))))
+
+(define %nginx-configuration
+  (nginx-configuration
+   (nginx %nginx-package)
+   (server-blocks
+    (list
+     (nginx-server-configuration
+      (server-name (list 'default))
+      (listen '("80 default_server" "[::]:80 default_server"))
+      (raw-content '("return 301 https://guix.org.cn/;")))
+
+     (nginx-server-configuration
+      (server-name (list "guix.org.cn"))
+      (listen '("443 ssl" "[::]:443 ssl"))
+      (ssl-certificate "/etc/letsencrypt/live/guix.org.cn/fullchain.pem")
+      (ssl-certificate-key "/etc/letsencrypt/live/guix.org.cn/privkey.pem")
+      (root "/srv/www/guix.org.cn")
+      (locations (list %nginx-status-stub-configuration))
+      (raw-content '("access_log /var/log/nginx/guix.org.cn.access.log;"
+                     "error_log /var/log/nginx/guix.org.cn.error.log;")))
+
+     (nginx-server-configuration
+      (server-name (list "ci.guix.org.cn"))
+      (listen '("443 ssl" "[::]:443 ssl"))
+      (ssl-certificate "/etc/letsencrypt/live/ci.guix.org.cn/fullchain.pem")
+      (ssl-certificate-key "/etc/letsencrypt/live/ci.guix.org.cn/privkey.pem")
+      (locations (list (nginx-location-configuration
+                        (uri "/")
+                        (body (list "proxy_pass http://localhost:8181;")))
+                       %nginx-status-stub-configuration))
+      (raw-content '("access_log /var/log/nginx/ci.guix.org.cn.access.log;"
+                     "error_log /var/log/nginx/ci.guix.org.cn.error.log;")))
+
+     (nginx-server-configuration
+      (server-name (list "mirror.guix.org.cn"))
+      (listen '("443 ssl" "[::]:443 ssl"))
+      (ssl-certificate "/etc/letsencrypt/live/mirror.guix.org.cn/fullchain.pem")
+      (ssl-certificate-key "/etc/letsencrypt/live/mirror.guix.org.cn/privkey.pem")
+      (locations `(;; Cuirass
+                   ,(nginx-location-configuration
+                     (uri "/")
+                     (body '("set $upstream \"https://ci.guix.gnu.org\";"
+                             "proxy_pass $upstream;"
+                             "proxy_ssl_server_name on;"
+                             "proxy_ssl_name ci.guix.gnu.org;"
+                             "proxy_set_header Host ci.guix.gnu.org;")))
+                   ;; Mirror
+                   ,@%guix-mirror-nginx-location-configurations
+                   ,%git-http-nginx-location-configuration
+                   ;; Status
+                   ,%nginx-status-stub-configuration))
+      (raw-content '("access_log /var/log/nginx/mirror.guix.org.cn.access.log;"
+                     "error_log /var/log/nginx/mirror.guix.org.cn.error.log;")))))
+   (extra-content "
+resolver 114.114.114.114 9.9.9.9 ipv6=off;
+
+# Cache for nar.
+proxy_cache_path /srv/cache/nginx/nar
+    levels=2
+    inactive=60d                # Remove inactive keys after this period.
+    keys_zone=nar:20m           # About 8 thousand keys per megabyte.
+    max_size=80g;               # Total cache data size.
+
+# Cache for narinfo.
+proxy_cache_path /srv/cache/nginx/narinfo
+    levels=2
+    inactive=60d                # Remove inactive keys after this period.
+    keys_zone=narinfo:20m       # About 8 thousand keys per megabyte.
+    max_size=1g;                # Total cache data size.
+")))
+
+(define %web-log-rotations
+  (list (log-rotation
+         (files (list "/var/log/nginx/*.log"))
+         (frequency 'daily)
+         (post-rotate #~(let ((pid (call-with-input-file "/var/run/nginx/pid"
+                                     read)))
+                          (kill pid SIGUSR1)))
+         (options '("nostoredir"
+                    "storefile @BASENAME-@YEAR@MONTH@DAY.@COMP_EXT")))))
+
+(define %web-services
+  (list (service certbot-service-type %certbot-configuration)
+        (service fcgiwrap-service-type)
+        (service nginx-service-type %nginx-configuration)
+        (simple-service 'git-repository-mirror-jobs
+                        mcron-service-type
+                        %git-repository-mirror-jobs)
+        (simple-service 'web-log-rotations
+                        rottlog-service-type
+                        %web-log-rotations)))
