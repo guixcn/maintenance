f8c8de682f6ab2b22eca8b51b8c68d1e8459f05e
Peng Mei Yu <pengmeiyu@riseup.net>
2021-02-22 14:51:54 +0800

    nanjing, shanghai: Remove Guix CI mirror.

--- nanjing/web.scm
+++ nanjing/web.scm
@@ -1,4 +1,4 @@
-;;; Copyright © 2020 Peng Mei Yu <i@pengmeiyu.com>
+;;; Copyright © 2020-2021 Peng Mei Yu <i@pengmeiyu.com>
 
 (use-modules (gnu packages version-control)
              (gnu packages web)
@@ -52,68 +52,6 @@
                                "--git-dir=/srv/git/guix.git"
                                "fetch"))))))
 
-(define %guix-ci-upstream-configuration
-  (nginx-upstream-configuration
-   (name "guix-ci")
-   ;; Repeat server here to force nginx to retry this server when there is an
-   ;; error.
-   (servers '("guix-mirror.pengmeiyu.com:443 max_fails=0"
-              "guix-mirror.pengmeiyu.com:443 max_fails=0"
-              "guix-mirror.pengmeiyu.com:443 max_fails=0"
-              "guix-mirror.pengmeiyu.com:443 max_fails=0"))))
-
-(define %guix-mirror-nginx-location-configurations
-  (let* ((upstream "guix-ci")
-         (domain "guix-mirror.pengmeiyu.com"))
-    (list (nginx-location-configuration
-           (uri "~ \\.narinfo$")
-           (body (list
-                  (string-append "proxy_pass https://" upstream ";")
-                  (string-append "proxy_set_header Host " domain ";")
-                  "proxy_ssl_server_name on;"
-                  (string-append "proxy_ssl_name " domain ";")
-
-                  ;; Try next upstream server when there is an error.
-                  "proxy_next_upstream error timeout invalid_header;"
-                  "proxy_next_upstream_timeout 20s;"
-
-                  ;; Die quickly and try next upstream server.
-                  "proxy_connect_timeout 2s;"
-                  "proxy_read_timeout 3s;"
-                  "proxy_send_timeout 2s;"
-
-                  "proxy_cache narinfo;"
-                  "proxy_cache_valid 200 206 60d;"
-                  "proxy_cache_valid any 5m;"
-
-                  "proxy_ignore_client_abort on;"
-                  "proxy_hide_header Set-Cookie;"
-                  "proxy_ignore_headers Set-Cookie;")))
-          (nginx-location-configuration
-           (uri "~ ^/nar/")
-           (body (list
-                  (string-append "proxy_pass https://" upstream ";")
-                  (string-append "proxy_set_header Host " domain ";")
-                  "proxy_ssl_server_name on;"
-                  (string-append "proxy_ssl_name " domain ";")
-
-                  ;; Try next upstream server when there is an error.
-                  "proxy_next_upstream error timeout invalid_header;"
-
-                  ;; Die quickly and try next upstream server.
-                  "proxy_connect_timeout 2s;"
-                  "proxy_read_timeout 30s;"
-                  "proxy_send_timeout 30s;"
-
-                  "proxy_cache nar;"
-                  "proxy_cache_valid 200 206 60d;"
-                  "proxy_cache_valid any 5m;"
-                  "gzip off;"
-
-                  "proxy_ignore_client_abort on;"
-                  "proxy_hide_header Set-Cookie;"
-                  "proxy_ignore_headers Set-Cookie;"))))))
-
 (define %nginx-status-stub-configuration
   (nginx-location-configuration
    (uri "/nginx_status")
@@ -145,39 +83,10 @@
       (listen '("443 ssl" "[::]:443 ssl"))
       (ssl-certificate "/etc/letsencrypt/live/mirror.guix.org.cn/fullchain.pem")
       (ssl-certificate-key "/etc/letsencrypt/live/mirror.guix.org.cn/privkey.pem")
-      (locations `(;; Cuirass
-                   ,(nginx-location-configuration
-                     (uri "/")
-                     (body '("set $upstream \"https://ci.guix.gnu.org\";"
-                             "proxy_pass $upstream;"
-                             "proxy_ssl_server_name on;"
-                             "proxy_ssl_name ci.guix.gnu.org;"
-                             "proxy_set_header Host ci.guix.gnu.org;")))
-                   ;; Mirror
-                   ,@%guix-mirror-nginx-location-configurations
-                   ,%git-http-nginx-location-configuration
-                   ;; Status
-                   ,%nginx-status-stub-configuration))
+      (locations (list %git-http-nginx-location-configuration
+                       %nginx-status-stub-configuration))
       (raw-content '("access_log /var/log/nginx/mirror.guix.org.cn.access.log;"
-                     "error_log /var/log/nginx/mirror.guix.org.cn.error.log;")))))
-   (upstream-blocks (list %guix-ci-upstream-configuration))
-   (extra-content "
-resolver 114.114.114.114 9.9.9.9 ipv6=off;
-
-# Cache for nar.
-proxy_cache_path /srv/cache/nginx/nar
-    levels=2
-    inactive=60d                # Remove inactive keys after this period.
-    keys_zone=nar:20m           # About 8 thousand keys per megabyte.
-    max_size=30g;               # Total cache data size.
-
-# Cache for narinfo.
-proxy_cache_path /srv/cache/nginx/narinfo
-    levels=2
-    inactive=60d                # Remove inactive keys after this period.
-    keys_zone=narinfo:20m       # About 8 thousand keys per megabyte.
-    max_size=1g;                # Total cache data size.
-")))
+                     "error_log /var/log/nginx/mirror.guix.org.cn.error.log;")))))))
 
 (define %web-log-rotations
   (list (log-rotation
