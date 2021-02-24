58e1ecf354d943f9d294778f41c4974bf7f5b65c
qblade <qblade@protonmail.com>
2021-02-24 14:39:54 +0000

    shanghai: add monkey qblade
    * shanghai/monkeys.scm (qblade): New monkey.

--- shanghai/monkeys.scm
+++ shanghai/monkeys.scm
@@ -9,4 +9,10 @@
          (shell (file-append zsh "/bin/zsh"))
          (ssh-public-key
           (plain-file "meiyu.pub"
-                      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGixg7L7vRFgmxBS2GmI4/UqPw7pERi3qbKFUPaEZIF")))))
+                      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGixg7L7vRFgmxBS2GmI4/UqPw7pERi3qbKFUPaEZIF")))
+	(monkey
+         (name "qblade")
+         (comment "luhux")
+         (ssh-public-key
+          (plain-file "luhux.pub"
+                      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIbLzIHSgPsTHirnDDVteW8gcumLnzizb05syPgLiDve")))))
