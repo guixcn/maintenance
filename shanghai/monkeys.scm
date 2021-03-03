032a22c4da0b8000f39c700530ffd59aaa7c1283
PandaGix <78696498+PandaGix@users.noreply.github.com>
2021-03-03 14:22:05 +0800

    shanghai: Add monkey pandagix.

--- shanghai/monkeys.scm
+++ shanghai/monkeys.scm
@@ -10,9 +10,15 @@
          (ssh-public-key
           (plain-file "meiyu.pub"
                       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGixg7L7vRFgmxBS2GmI4/UqPw7pERi3qbKFUPaEZIF")))
-	(monkey
+        (monkey
          (name "qblade")
          (comment "luhux")
          (ssh-public-key
           (plain-file "luhux.pub"
-                      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIbLzIHSgPsTHirnDDVteW8gcumLnzizb05syPgLiDve")))))
+                      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIbLzIHSgPsTHirnDDVteW8gcumLnzizb05syPgLiDve")))
+        (monkey
+         (name "pandagix")
+         (comment "PandaGix")
+         (ssh-public-key
+          (plain-file "pandagix.pub"
+                      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPZ3/HBVQ+t8mtGuYXJUbbKR8yynheYl3RpbIs82ANv2")))))
